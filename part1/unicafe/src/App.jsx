775bcc3744e3612fd7e7e83bfad5c43bca2eb9cc
AndreMatevosyan <andre12231205@gmail.com>
2023-12-09 21:17:42 -0800

    Finished 1c

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
+const Display = ({counter}) => {
   return (
     <div>{counter}</div>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+  <button onClick={onClick}>
+    {text}
+  </button>
+  )
+}
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const setToZero = () => setCounter(0)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='Add'/>
+      <Button onClick={decreaseByOne} text='Subtract'/>
+      <Button onClick={setToZero} text='Reset' />
+    </div>
   )
 }
 
