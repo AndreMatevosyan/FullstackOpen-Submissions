cab498b5450d06b0b4ae861e184261465ff81f23
AndreMatevosyan <andre12231205@gmail.com>
2024-02-01 22:02:20 -0800

    Finished part 3

--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -1,38 +1,42 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = 
-    `mongodb://andre12231205:${password}@ac-9a3hvp3-shard-00-00.o8fyaco.mongodb.net:27017,ac-9a3hvp3-shard-00-01.o8fyaco.mongodb.net:27017,ac-9a3hvp3-shard-00-02.o8fyaco.mongodb.net:27017/noteApp?ssl=true&replicaSet=atlas-fdk6if-shard-0&authSource=admin&retryWrites=true&w=majority`
- 
-mongoose.set('strictQuery', false)
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
-//     content: 'HTML is Easy',
-//     important: true,
+//   content: 'Mongoose makes things easy',
+//   date: new Date(),
+//   important: true,
 // })
 
-// note.save().then(result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
