2d39244f53a653e258a56b6efa93f65b98c99c6b
AndreMatevosyan <andre12231205@gmail.com>
2024-03-22 10:54:34 -0700

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,24 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useRef } from "react"
+import MoreInfo from "./MoreInfo"
+
+const Blog = ({ blog }) => {
+  
+  const infoRef = useRef()
+
+  const changeView = () => {
+    infoRef.current.toggleVisibility()
+  }
+
+  return (
+  <div className="simpleBorder">
     {blog.title} {blog.author}
+    <button onClick={changeView}>view</button>
+    <MoreInfo ref={infoRef}>
+      {blog.url}<br/>
+      likes {blog.likes} <button>like</button><br/>
+      {blog.user.name}<br/>
+    </MoreInfo>
   </div>  
-)
+)}
 
 export default Blog
---
+++ part5/bloglist-frontend/src/components/MoreInfo.jsx
@@ -0,0 +1,26 @@
+import { useImperativeHandle, forwardRef, useState } from 'react'
+
+const MoreInfo = forwardRef((props, refs) => {
+
+    const [showView, setShowView] = useState(false)
+
+    const detailsStyle = showView ? {display: ''} : {display: 'none'}
+
+    const toggleVisibility = () => {
+        setShowView(!showView)
+    }
+
+    useImperativeHandle(refs, () => {
+        return {
+            toggleVisibility
+        }
+    })
+
+    return (
+        <div style={detailsStyle}>
+            {props.children}
+        </div>
+    )
+})
+
+export default MoreInfo
